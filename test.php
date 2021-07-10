d2e9d54bc5f43e8cc976e7d30c21ab4823d56f90
Martin Folkers <hello@twobrain.io>
2021-07-10 12:21:01 +0200

    Update test suite

--- test.php
+++ test.php
@@ -3,35 +3,47 @@
 require_once('vendor/autoload.php');
 
 
-$drivers = [
-    'composer',
-    'yarn1',
-    'yarn2',
-    'npm',
-];
-
-if (in_array($argv[1], $drivers) === false) {
-    throw new Exception('Invalid driver, exiting ..');
-}
-
-
-switch ($argv[1]) {
-    case 'composer':
+if (isset($argv[1])) {
+    if ($argv[1] === 'composer') {
         $obj = new S1SYPHOS\Thx('tests/composer/composer.json', 'tests/composer/composer.lock');
+    }
 
-    case 'yarn1':
+    if ($argv[1] === 'yarn1') {
         $obj = new S1SYPHOS\Thx('tests/yarn-v1/package.json', 'tests/yarn-v1/yarn.lock');
+    }
 
-    case 'yarn2':
+    if ($argv[1] === 'yarn2') {
         $obj = new S1SYPHOS\Thx('tests/yarn-v2/package.json', 'tests/yarn-v2/yarn.lock');
+    }
 
-    case 'npm':
+    if ($argv[1] === 'npm') {
         $obj = new S1SYPHOS\Thx('tests/npm/package.json', 'tests/npm/package-lock.json');
+    }
 }
 
 
-var_dump($obj->data());
+if (!isset($obj)) {
+    echo sprintf('Invalid driver, exiting ..%s', "\n");
+    exit(1);
+}
 
 
-# Extend proper geeting
-echo sprintf('Loading tests for %s .. done.%s', $argv[1], "\n");
+if (isset($argv[2])) {
+    # Extend proper geeting
+    echo sprintf('Loading tests for "%s" ..%s', $argv[1], "\n");
+
+    if ($argv[2] === 'data') {
+        var_dump($obj->data());
+    }
+
+    if ($argv[2] === 'pkgs') {
+        var_dump($obj->pkgs());
+    }
+
+    if ($argv[2] === 'packages') {
+        var_dump($obj->packages());
+    }
+
+} else {
+    echo sprintf('Invalid data source for "%s", exiting ..%s', $argv[1], "\n");
+}
