9102ed000e2b98bfde19790870763a74421f5af2
Martin Folkers <hello@twobrain.io>
2021-07-11 08:58:55 +0200

    Change 'Thx' class into factory

--- lib/Drivers/Node.php
+++ lib/Drivers/Node.php
@@ -21,6 +21,10 @@ class Node extends Driver
 
 
     /**
+     * Methods
+     */
+
+    /**
      * Extracts raw data from input files
      *
      * @param string $dataFile Path to data file
@@ -55,10 +59,60 @@ class Node extends Driver
     protected function process(): array
     {
         return array_map(function($pkgName, $pkg) {
-            return [
-                'name' => $pkgName,
-                'version' => $pkg['version'],
-            ];
+            return $this->_process($pkgName, $pkg);
         }, array_keys($this->data), $this->data);
+    }
+
+
+    /**
+     * Processes raw data
+     *
+     * @return array Processed data
+     */
+    protected function _process(string $pkgName, array $pkg): array
+    {
+        $data = [];
+
+        # Build unique caching key
+        $hash = md5($pkgName);
+
+        # Fetch information about package ..
+        if ($this->cache->has($hash)) {
+            # (1) .. from cache (if available)
+            $data = $this->cache->get($hash);
+
+            $this->fromCache = true;
+        }
+
+        if (empty($data)) {
+            # (2) .. from API
+            # Block unwanted libraries
+            if (in_array($pkgName, $this->blockList) === true) return false;
+
+            # Prepare data for each repository
+            $data['name'] = $pkgName;
+            $data['version'] = $pkg['version'];
+
+            # Fetch additional information from https://api.npms.io
+            $response = $this->fetchRemote('https://api.npms.io/v2/package/' . rawurlencode($pkgName));
+            $response = json_decode($response)->collected->metadata;
+
+            $data['license'] = $response->license ?? '';
+            $data['description'] = $response->description;
+            $data['url'] = $response->links->repository;
+            $data['forked'] = false;
+
+            # Check if it's a forked repository
+            if (preg_match('/(([0-9])+(\.{0,1}([0-9]))*)/', $data['version']) == false) {
+                # TODO: Check if that's even a thing
+                # $data['version'] = $data->version;
+                $data['forked'] = true;
+            }
+
+            # Cache result
+            $this->cache->set($hash, $data, $this->days2seconds($this->cacheDuration));
+        }
+
+        return $data;
     }
 }
